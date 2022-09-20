2911ad55efb8201b6d7d31f3e36752e2484f4995
Ilya <Ermolaev.ID@yandex.ru>
2022-09-20 16:42:29 +0500

    Update copy-object.test.js

--- 6-task/tests/copy-object.test.js
+++ 6-task/tests/copy-object.test.js
@@ -1,23 +1,73 @@
 import { strict as assert } from "assert";
-import { copyObject } from "../solution.js";
+import { copyObject, isCopy } from "../solution.js";
 
-const testObject = {
-    a: 'stringExample',
-    b: 5,
-    c: true,
-    d: {
-        a: 'stringExample',
-        b: 5,
-        c: false,
-        d: () => console.log('Я вложенная функция'),
-    },
-    e: (lol, kek) => {
-        console.log(lol, kek)
-    },
+
+function publicTest1() {
+    const source = 5;
+    const copy = copyObject(source);
+    return JSON.stringify(source) === JSON.stringify(copy) && isCopy(source, copy);
 }
 
-export const simpleCopyTest = () => {
-    const copy = copyObject(testObject);
+function publicTest2() {
+    const source = 'test string';
+    const copy = copyObject(source);
+    return JSON.stringify(source) === JSON.stringify(copy) && isCopy(source, copy);
+}
 
-    assert.equal(JSON.stringify(testObject) === JSON.stringify(copy), true);
+function publicTest3() {
+    const source = (a) = console.log(a);
+    const copy = copyObject(source);
+    return JSON.stringify(source) === JSON.stringify(copy) && isCopy(source, copy);
+}
+
+function publicTest4() {
+    const source = {
+        a: 'stringExample',
+        b: 5,
+        c: true,
+        d: {
+            a: 'stringExample',
+            b: 5,
+            c: false,
+            d: () => console.log('Я вложенная функция'),
+        },
+        e: (lol, kek) => {
+            console.log(lol, kek);
+        },
+    };
+    const copy = copyObject(source);
+    return JSON.stringify(source) === JSON.stringify(copy) && isCopy(source, copy);
+}
+
+/** Запуск всех тестов с выводом результата */
+export function startAllTests() {
+    const results = [
+        publicTest1(),
+        publicTest2(),
+        publicTest3(),
+        publicTest4()
+    ];
+    
+    const allTestsCount = results.length;
+    const mapFunc = (arr) => arr
+        .map((result, index) => {
+            return {
+                result,
+                index: index + 1
+            }
+        });
+    const successTests = mapFunc(results)
+        .filter((result) => result.result)
+        .map((result) => result.index);
+    
+    const errorTests = mapFunc(results)
+        .filter((result) => !result.result)
+        .map((result) => result.index);
+
+    console.log(`Test count: ${results.length}`);
+    console.log(`Success: ${successTests}`);
+    console.log(`Error: ${errorTests}`);
+    console.log(`Tests passed: ${allTestsCount === successTests.length}`);
+    
+    assert.equal(allTestsCount === successTests.length, true);
 }
